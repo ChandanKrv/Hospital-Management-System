8b6af699719713e64b3df8ebc88d03163ba74dac
ChandanKrv <chandan31jan97@gmail.com>
2021-04-30 16:03:34 +0530

    addmission

--- patient/admission.php
+++ patient/admission.php
@@ -1,84 +1,88 @@
 <?php include_once('patient_header.php') ?>
 <div class="container-fluid">
-                    <div class="layout-specing">
-                        <div class="row">
-                            <div class="col-xl-9 col-lg-6 col-md-4">
-                                <h5 class="mb-0">Admission</h5>
-                                <nav aria-label="breadcrumb" class="d-inline-block mt-2">
-                                    <ul class="breadcrumb breadcrumb-muted bg-transparent rounded mb-0 p-0">
-                                        <li class="breadcrumb-item"><a href="index">Doctris</a></li>
-                                        <li class="breadcrumb-item active" aria-current="page">Admission</li>
-                                    </ul>
-                                </nav>
-                            </div><!--end col-->
+    <div class="layout-specing">
+        <div class="row">
+            <div class="col-xl-9 col-lg-6 col-md-4">
+                <h5 class="mb-0">Admission</h5>
+                <nav aria-label="breadcrumb" class="d-inline-block mt-2">
+                    <ul class="breadcrumb breadcrumb-muted bg-transparent rounded mb-0 p-0">
+                        <li class="breadcrumb-item"><a href="index">Doctris</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Admission</li>
+                    </ul>
+                </nav>
+            </div>
+            <!--end col-->
 
-                            <div class="col-xl-3 col-lg-6 col-md-8 mt-4 mt-md-0">
-                                <div class="justify-content-md-end">
-                                    
-                                </div>
-                            </div><!--end col-->
-                        </div><!--end row-->
-                        
-                        <div class="row">
-                            <div class="col-12 mt-4">
-                                <div class="table-responsive shadow rounded">
-                                    <table class="table table-center bg-white mb-0">                                   
-                                        <thead>
-                                            <tr>
-                                                <th class="border-bottom py-3" style="min-width: 50px;">#</th>
-                                                <th class="border-bottom py-3" style="min-width: 180px;">Name</th>
-                                                <th class="border-bottom py-3" style="min-width: 150px;">Email</th>
-                                                <th class="border-bottom py-3">Age</th>
-                                                <th class="border-bottom py-3">Gender</th>
-                                                <th class="border-bottom py-3">Department</th>
-                                                <th class="border-bottom py-3" style="min-width: 150px;">Date</th>                                                
-                                                <th class="border-bottom py-3" style="min-width: 220px;">Doctor</th>                                                
-                                                <th class="border-bottom py-3" style="min-width: 150px;"></th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr>
-                                                <th>1</th>
-                                                <td class="py-3">
-                                                    <a href="#" class="text-dark">
-                                                        <div class="d-flex align-items-center">
-                                                            <img src="../assets/images/client/01.jpg" class="avatar avatar-md-sm rounded-circle shadow" alt="">
-                                                            <span class="ms-2">Howard Tanner</span>
-                                                        </div>
-                                                    </a>
-                                                </td>
-                                                <td>howard@contact.com</td>
-                                                <td>25</td>
-                                                <td>Male</td>
-                                                <td>Cardiology</td>
-                                                <td>20th Dec 2020</td>                                                
-                                                <td>
-                                                    <a href="#" class="text-dark">
-                                                        <div class="d-flex align-items-center">
-                                                            <img src="../assets/images/doctors/01.jpg" class="avatar avatar-md-sm rounded-circle border shadow" alt="">
-                                                            <span class="ms-2">Dr. Calvin Carlo</span>
-                                                        </div>
-                                                    </a>
-                                                </td>
-                                                
-                                                <td class="text-end">
-                                                <div class="d-grid">
-                                                    <a href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#admissionform">Edit</a>
-                                                </div>
-                                                    <!-- <a href="#" class="btn btn-icon btn-pills btn-soft-primary" data-bs-toggle="modal" data-bs-target="#viewadmission"><i class="uil uil-eye"></i></a>
+            <div class="col-xl-3 col-lg-6 col-md-8 mt-4 mt-md-0">
+                <div class="justify-content-md-end">
+
+                </div>
+            </div>
+            <!--end col-->
+        </div>
+        <!--end row-->
+        <a href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#admissionform">Edit</a>
+        <div class="row">
+            <div class="col-12 mt-4">
+                <div class="table-responsive shadow rounded">
+                    <table class="table table-center bg-white mb-0">
+                        <thead>
+                            <tr>
+                                <th class="border-bottom py-3" style="min-width: 50px;">#</th>
+                                <th class="border-bottom py-3" style="min-width: 180px;">Name</th>
+                                <th class="border-bottom py-3" style="min-width: 150px;">Email</th>
+                                <th class="border-bottom py-3">Age</th>
+                                <th class="border-bottom py-3">Gender</th>
+                                <th class="border-bottom py-3">Department</th>
+                                <th class="border-bottom py-3" style="min-width: 150px;">Date</th>
+                                <th class="border-bottom py-3" style="min-width: 220px;">Doctor</th>
+                                <th class="border-bottom py-3" style="min-width: 150px;"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th>1</th>
+                                <td class="py-3">
+                                    <a href="#" class="text-dark">
+                                        <div class="d-flex align-items-center">
+                                            <img src="../assets/images/client/01.jpg" class="avatar avatar-md-sm rounded-circle shadow" alt="">
+                                            <span class="ms-2">Howard Tanner</span>
+                                        </div>
+                                    </a>
+                                </td>
+                                <td>howard@contact.com</td>
+                                <td>25</td>
+                                <td>Male</td>
+                                <td>Cardiology</td>
+                                <td>20th Dec 2020</td>
+                                <td>
+                                    <a href="#" class="text-dark">
+                                        <div class="d-flex align-items-center">
+                                            <img src="../assets/images/doctors/01.jpg" class="avatar avatar-md-sm rounded-circle border shadow" alt="">
+                                            <span class="ms-2">Dr. Calvin Carlo</span>
+                                        </div>
+                                    </a>
+                                </td>
+
+                                <td class="text-end">
+                                    <div class="d-grid">
+                                        <a href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#admissionform">Edit</a>
+                                    </div>
+                                    <!-- <a href="#" class="btn btn-icon btn-pills btn-soft-primary" data-bs-toggle="modal" data-bs-target="#viewadmission"><i class="uil uil-eye"></i></a>
                                                     <a href="#" class="btn btn-icon btn-pills btn-soft-success" data-bs-toggle="modal" data-bs-target="#acceptadmission"><i class="uil uil-check-circle"></i></a>
                                                     <a href="#" class="btn btn-icon btn-pills btn-soft-danger" data-bs-toggle="modal" data-bs-target="#canceladmission"><i class="uil uil-times-circle"></i></a> -->
-                                                </td>
-                                            </tr>
-                                            
-                                             
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </div>
-                        </div><!--end row-->
+                                </td>
+                            </tr>
 
-                        <!-- <div class="row text-center">
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <!--end row-->
+
+        <!-- <div class="row text-center">
                             <div class="col-12 mt-4">
                                 <ul class="pagination justify-content-end mb-0 list-unstyled">
                                     <li><a href="#" class="pe-3 ps-3 pt-2 pb-2 border"> Prev</a></li>
@@ -91,8 +95,8 @@
                     </div>
                 </div><--end container-->
 
-<!-- MODAL START -->
- <!-- Modal start -->
+        <!-- MODAL START -->
+        <!-- Modal start -->
         <!-- Add New Admission Start -->
         <div class="modal fade" id="admissionform" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog modal-lg modal-dialog-centered">
@@ -106,39 +110,44 @@
                             <div class="row">
                                 <div class="col-lg-12">
                                     <div class="mb-3">
-                                    <input name="hms_id" id="name" type="text" class="form-control" placeholder="Patient Name :" value="<?php echo $hms_id ?>">
+                                        <input name="hms_id" id="name" type="text" class="form-control" placeholder="Patient Name :" value="<?php echo $hms_id ?>">
                                         <label class="form-label">Patient Name <span class="text-danger">*</span></label>
                                         <input name="name" id="name" type="text" class="form-control" placeholder="Patient Name :" value="<?php echo $u_full_name ?>" disabled>
                                     </div>
-                                </div><!--end col-->
-                                
+                                </div>
+                                <!--end col-->
+
                                 <div class="col-lg-6 col-md-9">
                                     <div class="mb-3">
                                         <label class="form-label">Select Doctor</label>
                                         <select class="form-control doctor-name select2input">
-                                        <?php getDropdownDoctor('admission'); ?>
+                                            <?php getDropdownDoctor('admission'); ?>
                                         </select>
                                     </div>
-                                </div><!--end col-->
+                                </div>
+                                <!--end col-->
                                 <div class="col-lg-6 col-md-9">
                                     <div class="mb-3">
                                         <label class="form-label"> Date : </label>
                                         <input name="date" type="date" class="flatpickr flatpickr-input form-control" id="checkin-date">
                                     </div>
-                                </div><!--end col-->
+                                </div>
+                                <!--end col-->
                                 <div class="col-lg-4 col-md-6">
                                     <div class="mb-3">
                                         <label class="form-label">Your Email <span class="text-danger">*</span></label>
                                         <input name="email" id="email" type="email" class="form-control" placeholder="Your email :" value="<?php echo $u_email ?>" disabled>
-                                    </div> 
-                                </div><!--end col-->
+                                    </div>
+                                </div>
+                                <!--end col-->
 
                                 <div class="col-lg-4 col-md-6">
                                     <div class="mb-3">
                                         <label class="form-label">Your Phone <span class="text-danger">*</span></label>
                                         <input name="phone" id="phone" type="tel" class="form-control" placeholder="Your Phone :" value="<?php echo $phone ?>" disabled>
-                                    </div> 
-                                </div><!--end col-->
+                                    </div>
+                                </div>
+                                <!--end col-->
 
 
                                 <div class="col-lg-12">
@@ -146,14 +155,17 @@
                                         <label class="form-label">Admission Reason<span class="text-danger">*</span></label>
                                         <textarea name="comments" id="comments" rows="4" class="form-control" placeholder="Your Message :"></textarea>
                                     </div>
-                                </div><!--end col-->
+                                </div>
+                                <!--end col-->
 
                                 <div class="col-lg-12">
                                     <div class="d-grid">
                                         <button type="submit" class="btn btn-primary">Edit Admission</button>
                                     </div>
-                                </div><!--end col-->
-                            </div><!--end row-->
+                                </div>
+                                <!--end col-->
+                            </div>
+                            <!--end row-->
                         </form>
                     </div>
                 </div>
@@ -264,4 +276,4 @@
         </div>
         <!-- Cancel Admission End -->
         <!-- Modal end -->
-<?php include_once('patient_footer.php') ?>
+        <?php include_once('patient_footer.php') ?>
