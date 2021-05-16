111ee25450d10f0cd6ae2288a96ef7b98fb9c1ee
ChandanKrv <chandan31jan97@gmail.com>
2021-05-16 11:08:39 +0530

    Login header 3

--- index/header.php
+++ index/header.php
@@ -1,14 +1,10 @@
 <?php
 session_start();
-error_reporting(E_ERROR | E_WARNING | E_PARSE);
-$email =  $_SESSION['email'];
-$u_role =  $_SESSION['u_role'];
-
-if (isset($email)) {
+if (isset($_SESSION['email']))
     $headerBtn="Dashboard";
-} else {
+ else 
     $headerBtn="Sign In";
-}
+
 ?>
 <!DOCTYPE html>
     <html lang="en">
